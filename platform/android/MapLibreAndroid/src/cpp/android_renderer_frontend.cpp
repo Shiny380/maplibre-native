8c228309a9d33604dccbf5a97247b0d374f7edd0
Shiny380 <daniel.mckinnell43@gmail.com>
2026-09-23 10:38:47 +1000

    Fix Android DEM query LatLng type

--- platform/android/MapLibreAndroid/src/cpp/android_renderer_frontend.cpp
+++ platform/android/MapLibreAndroid/src/cpp/android_renderer_frontend.cpp
@@ -175,7 +175,7 @@ std::vector<Feature> AndroidRendererFrontend::querySourceFeatures(const std::str
 }
 
 std::optional<double> AndroidRendererFrontend::queryRasterDEMElevation(const std::string& sourceID,
-                                                                       const LatLng& latLng) const {
+                                                                       const mln::LatLng& latLng) const {
     // Waits for the result from the orchestration thread and returns
     return mapRenderer.actor().ask(&Renderer::queryRasterDEMElevation, sourceID, latLng).get();
 }
